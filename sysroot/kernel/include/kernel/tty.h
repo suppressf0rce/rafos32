f91add2c3214f4f5290b9117a25c80589da87bff
suppressf0rce <suppressforce@gmail.com>
2018-03-30 23:50:26 +0000

    Updated license header

--- sysroot/kernel/include/kernel/tty.h
+++ sysroot/kernel/include/kernel/tty.h
@@ -1,3 +1,12 @@
+//
+// Copyright (c) 2018 by Dejan Radmanovic (SuppresSF0rcE).
+// All Rights Reserved.
+//
+// This file is part of RafOS and is released under the terms
+// of the NSCA License - See LICENSE.md for more info
+//
+
+
 #ifndef _KERNEL_TTY_H
 #define _KERNEL_TTY_H
 
--- sysroot/kernel/kernel/kernel.c
+++ sysroot/kernel/kernel/kernel.c
@@ -1,3 +1,12 @@
+//
+// Copyright (c) 2018 by Dejan Radmanovic (SuppresSF0rcE).
+// All Rights Reserved.
+//
+// This file is part of RafOS and is released under the terms
+// of the NSCA License - See LICENSE.md for more info
+//
+
+
 #include <stdio.h>
 #include <kernel/tty.h>
 
