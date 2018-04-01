ca502c78d0bd5219f82143309a6ea7221de5feeb
suppressf0rce <suppressforce@gmail.com>
2018-04-01 17:36:35 +0200

    More copyright

--- sysroot/libc/include/sys/cdefs.h
+++ sysroot/libc/include/sys/cdefs.h
@@ -1,3 +1,11 @@
+//
+// Copyright (c) 2018 by Dejan Radmanovic (SuppresSF0rcE).
+// All Rights Reserved.
+//
+// This file is part of RafOS and is released under the terms
+// of the NSCA License - See LICENSE.md for more info
+//
+
 #ifndef _SYS_CDEFS_H
 #define _SYS_CDEFS_H 1
 
