efe0e98f069c28d48d2e16759b56f3607539ddaa
suppressf0rce <suppressforce@gmail.com>
2018-04-05 15:36:09 +0200

    Refactored Kernel.c

--- sysroot/kernel/kernel/kernel.c
+++ sysroot/kernel/kernel/kernel.c
@@ -12,15 +12,32 @@
 #include <kernel/io.h>
 #include <kernel/gdt.h>
 
-/*=======================================================
- * Kernel main method. This method will be active
- * after the boot loader gives control to our os (kernel)
-=======================================================*/
-void kernel_main(void){
+
+/**=============================================================================
+ * kernel_early: this function will install most important components of kernel
+ * and will prepare kernel for the normal work of the system
+ *============================================================================*/
+void kernel_early(){
   terminal_initialize();
 	printf("Initialized TTY.\n");
 
   printf("Installing GDT...\n");
   gdt_install();
 
+  printf("Welcome to the RafOS!\n");
+}
+
+/*=======================================================
+ * Kernel main method. This method will be active
+ * after the boot loader gives control to our os (kernel)
+=======================================================*/
+void kernel_main(void){
+
+  //Calling kernel_early function
+  kernel_early();
+
+  printf("Kernel Shell has not been implemented yet. Halting!");
+  for(;;){
+    asm("hlt");
+  }
 }
